41dd618a3dad6c1855c456493c0791e04c9a8068
“Julia <juliadoxagarat@gmail.com>
2026-09-25 14:13:20 -0300

    agrego animacion de loading en home

--- js/home.js
+++ js/home.js
@@ -1,3 +1,46 @@
+// =========================================================
+// LOADING
+// =========================================================
+
+const loading = document.querySelector("#loading");
+
+const loadingProgreso = document.querySelector("#loading-progreso");
+
+const loadingPorcentaje = document.querySelector("#loading-porcentaje");
+
+
+let porcentaje = 0;
+
+
+// aumenta 1% cada 50 milisegundos
+// 100 x 50ms = 5000ms = 5 segundos
+
+const intervaloLoading = setInterval(() => {
+
+    porcentaje++;
+
+
+    // actualiza el numero
+    loadingPorcentaje.textContent = porcentaje + "%";
+
+
+    // actualiza el ancho de la barra
+    loadingProgreso.style.width = porcentaje + "%";
+
+
+    // cuando llega a 100 termina el loading
+    if (porcentaje >= 100) {
+
+        clearInterval(intervaloLoading);
+
+        loading.hidden = true;
+
+        document.body.classList.remove("cargando");
+
+    }
+
+}, 15); //aca cambias si queres un tiempo de carga distinto
+
 //menu HAmburguesa
 const botonMenu = document.querySelector("[data-boton-menu]");
 const menuCategorias = document.querySelector("#menu-categorias");
